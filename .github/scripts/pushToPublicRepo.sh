fe40d14b24505d78688f6808b7cb661e9146da2c
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-24 20:38:10 -0400

    Update pushToPublicRepo.sh

--- .github/scripts/pushToPublicRepo.sh
+++ .github/scripts/pushToPublicRepo.sh
@@ -8,24 +8,6 @@ GITHUB_REPO="$3"
 HELM_QTEST_VERSION="$4"
 QTEST_MGR_APP_VERSION="$5"
 
-HELM_INSIGHTS_VERSION="$6"
-INSIGHTS_APP_VERSION="$7"
-
-HELM_LAUNCH_VERSION="$8"
-LAUNCH_APP_VERSION="$9"
-
-HELM_PARAMETERS_VERSION="$10"
-PARAMETERS_APP_VERSION="$11"
-
-HELM_PULSE_VERSION="$12"
-PULSE_APP_VERSION="$13"
-
-HELM_SCENARIO_VERSION="$14"
-SCENARIO_APP_VERSION="$15"
-
-HELM_SESSION_VERSION="$16"
-SESSION_APP_VERSION="$17"
-
 CLONE_DIR=output_clone
 echo 'check current directory'
 #make directory
@@ -38,7 +20,7 @@ apt-get update && apt-get install git
 
 git config --global user.email "mtricentis@g.com"
 git config --global user.name "$GITHUB_USERNAME"
-git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/qtest-chart.git" $CLONE_DIR
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/qtest_public.git" $CLONE_DIR
 
 echo 'check current directory after Clone'
 cd $CLONE_DIR
@@ -49,30 +31,6 @@ ls -ltr
 #Update qtestManger Helm and app verison
 sed -i 's/\(.*version:.*\)/version: '"$HELM_QTEST_VERSION"'/g' Charts/qtest-chart/chart.yml
 sed -i 's/\(.*appVersion:.*\)/appVersion: '"$QTEST_MGR_VERSION"'/g' Charts/qtest-chart/chart.yml
-
-#Update insights Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_INSIGHTS_VERSION"'/g' Charts/qtest-insights/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$INSIGHTS_APP_VERSION"'/g' Charts/qtest-insights/chart.yml
-
-#Update qtest-launch Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_LAUNCH_VERSION"'/g' Charts/qtest-launch/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$LAUNCH_APP_VERSION"'/g' Charts/qtest-launch/chart.yml
-
-#Update qtest-parameters Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_PARAMETERS_VERSION"'/g' Charts/qtest-parameters/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$PARAMETERS_APP_VERSION"'/g' Charts/qtest-parameters/chart.yml
-
-#Update qtest-pulse Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_PULSE_VERSION"'/g' Charts/qtest-pulse/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$PULSE_APP_VERSION"'/g' Charts/qtest-pulse/chart.yml
-
-#Update qtest-scenario Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_SCENARIO_VERSION"'/g' Charts/qtest-scenario/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$SCENARIO_APP_VERSION"'/g' Charts/qtest-scenario/chart.yml
-
-#Update qtest-session Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_SESSION_VERSION"'/g' Charts/qtest-session/chart.yml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$SESSION_APP_VERSION"'/g' Charts/qtest-session/chart.yml
 
 git add --all
 git commit --message "Update from $GITHUB_REPOSITORY"
