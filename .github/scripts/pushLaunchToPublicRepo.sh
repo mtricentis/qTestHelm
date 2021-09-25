8f6bac4508dcd212568ba4742d84d881bd576a77
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-24 21:34:04 -0400

    Update pushLaunchToPublicRepo.sh

--- .github/scripts/pushLaunchToPublicRepo.sh
+++ .github/scripts/pushLaunchToPublicRepo.sh
@@ -29,8 +29,8 @@ git checkout -b $GITHUB_REPO
 cp -r $FOLDER/* .
 ls -ltr
 #Update Launch Helm and app verison
-sed -i 's/\(.*version:.*\)/version: '"$HELM_LAUNCH_VERSION"'/g' Charts/qtest-chart/Chart.yaml
-sed -i 's/\(.*appVersion:.*\)/appVersion: '"$LAUNCH_APP_VERSION"'/g' Charts/qtest-chart/Chart.yaml
+sed -i 's/\(.*version:.*\)/version: '"$HELM_LAUNCH_VERSION"'/g' Charts/qtest-launch/Chart.yaml
+sed -i 's/\(.*appVersion:.*\)/appVersion: '"$LAUNCH_APP_VERSION"'/g' Charts/qtest-launch/Chart.yaml
 
 git add --all
 git commit --message "Update from $GITHUB_REPOSITORY"
