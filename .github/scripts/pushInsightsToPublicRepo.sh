06e32e43436f714439bebce211af93a2aad48756
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-24 21:32:03 -0400

    Update pushInsightsToPublicRepo.sh

--- .github/scripts/pushInsightsToPublicRepo.sh
+++ .github/scripts/pushInsightsToPublicRepo.sh
@@ -28,7 +28,7 @@ echo "After cd $CLONE_DIR"
 git checkout -b $GITHUB_REPO
 cp -r $FOLDER/* .
 ls -ltr
-#Update qtestManger Helm and app verison
+#Update Insights Helm and app verison
 sed -i 's/\(.*version:.*\)/version: '"$HELM_INSIGHTS_VERSION"'/g' Charts/qtest-chart/Chart.yaml
 sed -i 's/\(.*appVersion:.*\)/appVersion: '"$INSIGHTS_APP_VERSION"'/g' Charts/qtest-chart/Chart.yaml
 
