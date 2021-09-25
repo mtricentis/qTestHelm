6fe0907a8cc8810c41226b6e7e85cc9cc849f7f9
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-24 22:36:25 -0400

    Update ParametersPushToPublicRepo.sh

--- .github/scripts/ParametersPushToPublicRepo.sh
+++ .github/scripts/ParametersPushToPublicRepo.sh
@@ -29,7 +29,7 @@ git checkout -b $GITHUB_REPO
 cp -r $FOLDER/Charts/qtest-parameters/* Charts/qtest-parameters
 ls -ltr
 #Update Parameters Helm and app verison
-sed -i '0,/^\([[:space:]]*version: *\)/version: '"$HELM_PARAMETERS_VERSION"'/g' Charts/qtest-parameters/Chart.yaml
+sed -i '0/\(version:.*\)/version: '"$HELM_PARAMETERS_VERSION"'/g' Charts/qtest-parameters/Chart.yaml
 sed -i 's/\(.*appVersion:.*\)/appVersion: '"$PARAMETERS_APP_VERSION"'/g' Charts/qtest-parameters/Chart.yaml
 
 git add --all
