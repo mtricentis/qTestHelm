eb0beab7d77260341f4f77a0f3cfc1110ed4db28
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-10-27 20:14:47 -0400

    Update qTestPushToPublicRepo.sh

--- .github/scripts/qTestPushToPublicRepo.sh
+++ .github/scripts/qTestPushToPublicRepo.sh
@@ -5,8 +5,11 @@ FOLDER="$1"
 GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 
+
 HELM_QTEST_VERSION="$4"
 QTEST_MGR_APP_VERSION="$5"
+PUBLIC_REPO="$6"
+APPLICATION_NAME="$7"
 
 CLONE_DIR=output_clone
 echo 'check current directory'
@@ -20,17 +23,19 @@ apt-get update && apt-get install git
 
 git config --global user.email "mtricentis@g.com"
 git config --global user.name "$GITHUB_USERNAME"
-git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/qtest_public.git" $CLONE_DIR
+#git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/qtest_public.git" $CLONE_DIR
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$PUBLIC_REPO.git" $CLONE_DIR
 
 echo 'check current directory after Clone'
 cd $CLONE_DIR
 echo "After cd $CLONE_DIR"
 git checkout -b $GITHUB_REPO
-cp -r $FOLDER/Charts/qtest-chart/* Charts/qtest-chart
+#cp -r $FOLDER/Charts/qtest-chart/* Charts/qtest-chart
+cp -r $FOLDER/Charts/$APPLICATION_NAME/* Charts/$APPLICATION_NAME
 ls -ltr
 #Update qtestManger Helm and app verison
-sed -i 's/\(^version:.*\)/version: '"$HELM_QTEST_VERSION"'/g' Charts/qtest-chart/Chart.yaml
-sed -i 's/\(^appVersion:.*\)/appVersion: '"$QTEST_MGR_APP_VERSION"'/g' Charts/qtest-chart/Chart.yaml
+sed -i 's/\(^version:.*\)/version: '"$HELM_QTEST_VERSION"'/g' Charts/$APPLICATION_NAME/Chart.yaml
+sed -i 's/\(^appVersion:.*\)/appVersion: '"$QTEST_MGR_APP_VERSION"'/g' Charts/$APPLICATION_NAME/Chart.yaml
 
 git add --all
 git commit --message "Update from $GITHUB_REPOSITORY"
